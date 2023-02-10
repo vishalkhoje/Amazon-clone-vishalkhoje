db2712ccb8673f8a121242372b04e29c278603c8
Vishal Khoje <36984676+vishalkhoje@users.noreply.github.com>
2023-02-10 14:46:29 +0530

    Update next.config.js
    Domain added in nextjs configuration

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     images: {
-        domains: ["links.papareact.com", "fakestoreapi.com", "dl.airtable.com", "v5.airtableusercontent.com"]
+        domains: ["links.papareact.com", "fakestoreapi.com", "dl.airtable.com", "v5.airtableusercontent.com", "images2.imgbox.com"]
     },
     env: {
         stripe_public_key: process.env.STRIPE_PUBLIC_KEY,
